17e5ff90d04abc5b66cb3d2150b8122027efd4e7
sandyliu418 <sandyliu418@gmail.com>
2023-10-24 20:09:30 -0700

    did more unit tests and created some individual cat data

--- server/test/models/cat.js
+++ server/test/models/cat.js
@@ -8,18 +8,38 @@ import models from '../../models/index.js';
 
 describe('models.Cat', () => {
   beforeEach(async () => {
-    await helper.loadFixtures([]);
+    await helper.loadFixtures(['cats']);
   });
 
   it('creates a new Cat record', async () => {
-    assert.deepStrictEqual(await models.Cat.count(), 0);
+    assert.deepStrictEqual(await models.Cat.count(), 3);
     const record = await models.Cat.create({
       Breed: 'This is a test title',
       MinYears: 5,
     });
-    assert.deepStrictEqual(await models.Cat.count(), 1);
+    assert.deepStrictEqual(await models.Cat.count(), 4);
     assert.notDeepStrictEqual(record.id, null);
     assert.deepStrictEqual(record.Breed, 'This is a test title');
     assert.deepStrictEqual(record.MinYears, 5);
   });
+
+  it('finds an Cat record by its id', async () => {
+    const record = await models.Cat.findByPk(10001);
+    assert.notDeepStrictEqual(record, null);
+    assert.deepStrictEqual(record.Breed, "DS1");
+  })
+
+  it('finds multiple Cat records', async () => {
+    const records = await models.Cat.findAll({
+      order: [['Breed', 'ASC']]
+    });
+    assert.deepStrictEqual(records.length, 3)
+  })
+
+  it('delete an Cat record', async () => {
+    assert.deepStrictEqual(await models.Cat.count(), 3);
+    const record = await models.Cat.findByPk(10001);
+    await record.destroy();
+    assert.deepStrictEqual(await models.Cat.count(), 2);
+  })
 });
